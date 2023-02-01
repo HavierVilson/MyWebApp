062aa262bc9e0f6662056546f1a269e7b735bbef
HavierVislon <haviervilson@gmail.com>
2023-02-01 21:48:34 +0300

    Lesson 4-5

--- src/main/java/ru/haviervilson/webapp/RockMusic.java
+++ src/main/java/ru/haviervilson/webapp/RockMusic.java
@@ -1,6 +1,14 @@
 package ru.haviervilson.webapp;
 
 public class RockMusic implements Music{
+    public void doMyInit() {
+        System.out.println("My Init");
+    }
+
+    public void doMyDestroy() {
+        System.out.println("My Destroy");
+    }
+
     @Override
     public String getSong() {
         return "Toxic city";
--- src/main/java/ru/haviervilson/webapp/TestSpring.java
+++ src/main/java/ru/haviervilson/webapp/TestSpring.java
@@ -6,13 +6,24 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
+//        Lesson 5
+
+//        RockMusic rockMusic = context.getBean("musicBean", RockMusic.class);
+//        System.out.println(rockMusic.getSong());
+
+        ClassicMusic classicMusic = context.getBean("musicBean", ClassicMusic.class);
+        System.out.println(classicMusic.getSong());
+
 //        Lesson 2
+
 //        Music music = context.getBean("musicBean", Music.class);
 //        MusicPlayer musicPlayer = new MusicPlayer(music);
 
 //        Lesson 3
-        MusicPlayer musicPlayer = context.getBean("musicPlayer",MusicPlayer.class);
-        musicPlayer.playMusic();
+
+//        MusicPlayer musicPlayer = context.getBean("musicPlayer",MusicPlayer.class);
+//        musicPlayer.playMusic();
+
         context.close();
     }
 }
